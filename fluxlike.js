fe4fbc697d23473dc48da487016b58899a1d6a16
danjamin <e051ba3be@gmail.com>
2015-09-17 20:20:52 -0700

    Export API and Store

--- fluxlike.js
+++ fluxlike.js
@@ -1,5 +1,6 @@
 import ActionCreators from './lib/ActionCreators.js';
 import ActionTypes from './lib/ActionTypes.js';
+import API from './lib/API.js';
 import bootstrap from './lib/bootstrap.js';
 import Dispatcher from './lib/Dispatcher.js';
 import LinkTo from './lib/LinkTo.js';
@@ -7,15 +8,18 @@ import RootView from './lib/RootView.js';
 import Router from './lib/Router.js';
 import RouteStore from './lib/RouteStore.js';
 import Serializer from './lib/Serializer.js';
+import Store from './lib/Store.js';
 
 export {
   ActionCreators,
   ActionTypes,
+  API,
   bootstrap,
   Dispatcher,
   LinkTo,
   RootView,
   Router,
   RouteStore,
-  Serializer
+  Serializer,
+  Store
 };
